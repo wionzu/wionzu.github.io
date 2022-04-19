6223c484cb9b911041c0f0bc4b0941d0bc07f7a8
wionzu <78271554+wionzu@users.noreply.github.com>
2022-04-19 17:52:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,108 +1,55 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ini_set('max_execution_time', 300); //300 seconds = 5 minutes. In case if your CURL is slow and is loading too much (Can be IPv6 problem)
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <title>Discord OAUTH</title>           
+    <style>
+      * {
+        text-align: center;
+        margin: 0px auto;
+      }
 
-error_reporting(E_ALL);
+      #welcome_txt {
+        font-size: 24px;
+      }
 
-define('OAUTH2_CLIENT_ID', '941234115150245919'); //Your client Id
-define('OAUTH2_CLIENT_SECRET', 'XS0VPgwdtvkJk695q9WxGxw9AFcZT6Ri'); //Your secret client code
+      #login-link {
+        background-color: whitesmoke;
+        border-radius: 8px;
+        color: black;
+        padding: 15px 30px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 20px;
+      }
 
-$authorizeURL = 'https://discordapp.com/api/oauth2/authorize';
-$tokenURL = 'https://discordapp.com/api/oauth2/token';
-$apiURLBase = 'https://discordapp.com/api/users/@me';
-
-session_start();
-
-// Start the login process by sending the user to Discord's authorization page
-if(get('action') == 'login') {
-
-  $params = array(
-    'client_id' => OAUTH2_CLIENT_ID,
-    'redirect_uri' => 'http://template3.ct8.pl/',
-    'response_type' => 'code',
-    'scope' => 'identify guilds'
-  );
-
-  // Redirect the user to Discord's authorization page
-  header('Location: https://discordapp.com/api/oauth2/authorize' . '?' . http_build_query($params));
-  die();
-}
-
-
-// When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
-if(get('code')) {
-
-  // Exchange the auth code for a token
-  $token = apiRequest($tokenURL, array(
-    "grant_type" => "authorization_code",
-    'client_id' => OAUTH2_CLIENT_ID,
-    'client_secret' => OAUTH2_CLIENT_SECRET,
-    'redirect_uri' => 'http://template3.ct8.pl/',
-    'code' => get('code')
-  ));
-  $logout_token = $token->access_token;
-  $_SESSION['access_token'] = $token->access_token;
-
-
-  header('Location: ' . $_SERVER['PHP_SELF']);
-}
-
-if(session('access_token')) {
-  $user = apiRequest($apiURLBase);
-
-  echo '<h3>Logged In</h3>';
-  echo '<h4>Welcome, ' . $user->username . '</h4>';
-  echo '<pre>';
-    print_r($user);
-  echo '</pre>';
-
-} else {
-  echo '<h3>Not logged in</h3>';
-  echo '<p><a href="?action=login">Log In</a></p>';
-}
-
-
-if(get('action') == 'logout') {
-  // This must to logout you, but it didn't worked(
-
-  $params = array(
-    'access_token' => $logout_token
-  );
-
-  // Redirect the user to Discord's revoke page
-  header('Location: https://discordapp.com/api/oauth2/token/revoke' . '?' . http_build_query($params));
-  die();
-}
-
-function apiRequest($url, $post=FALSE, $headers=array()) {
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-  $response = curl_exec($ch);
-
-
-  if($post)
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-
-  $headers[] = 'Accept: application/json';
-
-  if(session('access_token'))
-    $headers[] = 'Authorization: Bearer ' . session('access_token');
-
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-  $response = curl_exec($ch);
-  return json_decode($response);
-}
-
-function get($key, $default=NULL) {
-  return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
-}
-
-function session($key, $default=NULL) {
-  return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
-}
-
-?>
+      #login-link:hover {
+        cursor: pointer;
+      }
+    </style>
+  </head>
+  <body>
+    <br>
+    <a id='login-link' href='https://discord.com/api/oauth2/authorize?client_id=941234115150245919&redirect_uri=https%3A%2F%2Fwionzu.github.io%2F&response_type=code&scope=identify%20guilds'>Login with Discord</a><br><br>
+    <p id='welcome_txt'></p>
+    <script>
+      window.onload = function() {
+        if (location.href.indexOf("code") !== -1) { // Detect if you logged in or not
+          const code = location.href.substring(location.href.indexOf("code") + 5, location.href.length); // Get the code OAUTH gives you
+          const request = new XMLHttpRequest(); // Create a new XMLHttpRequest
+          request.open("POST", "http://localhost:7100/user"); // Open the XMLHttpRequest
+          request.send(code); // Send the code in the request
+          request.onload = () => { // Will run when the request is loaded
+            if (request.status === 500) { // Error
+              document.getElementById('welcome_txt').innerText = `There was an error with that. Please try logging in again. Error Code: ${request.status}`;
+            } else if (request.status === 200) { // Successful
+              document.getElementById("welcome_txt").innerText = `Welcome, ${req.responseText}!`
+            } else { // Other
+              document.getElementById('welcome_txt').innerText = `An error occured. Please try logging in again. Error Code: ${req.status}`;
+            }
+          }
+        }
+      }
+    </script>
+  </body>        
+</html>
